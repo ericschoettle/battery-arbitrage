6bdf26b95bd8e1ff892f27a245ada42e4b64fb5c
ericschoettle <eric.schoettle@gmail.com>
2019-03-24 13:56:27 -0700

    Mostly woring

--- index.js
+++ index.js
@@ -7,16 +7,46 @@ const filepath = process.env.FILEPATH || process.env.PWD;
 const inputFileName = process.env.INPUTFILENAME || 'dam.csv';
 const outputFileName = process.env.OUTPUTFILENAME || 'results.txt';
 
+// Todo: Reorganize into different objects, use scoping rather than passing variables. Maybe a battery object, or an hour-of-battery-capacity object?
+// Visualize!
+// Figure out how to do cash flow vs. profit - the energy in the battery has some value that you neglect with a purely cashflow analysis (as I'm doing)
+// Function to make a continuous function (from day-ahead-market) FIRST: Look at EIM prices and see if there are easy patterns to see. 
+// Function to pick from continuous function to make buy/sell on interals in another market (5 min Energy Imbalance Market).
+// Easy: Reserve 30-60 min capacity for EIM trading. Hard: Dynamically decide if one is likely to be more profitable than the other. 
+
+/* Possible constraints:
+1. Committments to future buy/sell. When do I decide what I'm going to do on the DAM?
+2. C rate. If I'm maxed out selling or buying on the DAM I presumably can't do the same action on the 15 min markets.
+  b. You can commit to buy into one and sell into the other, no physical asset needed. This forces the prices to converge. 
+Approaches: I can reserve some portion for each market, and test the different reserves. 
+I can make a plan and shift when I buy?
+
+
+*/
+
+/* Glossary
+
+See http://www.caiso.com/market/Pages/MarketProcesses.aspx and http://www.caiso.com/Pages/glossary.aspx
+
+LMP - Location Marginal Pricer
+FMM - Fifteen Minute Market
+RTD - Real-time Dispatch (5 min market?)
+HASP - Hour ahead scheduleing process. . 
+AS - Ancillary Services
+*/
+
 
 class PriceScheme {
   constructor() {
-    this.initialBatteryState = 3;//hours of charge
+    this.initialBatteryState = 0;//hours of charge
     this.initialMoney = 0; // 
-    this.batteryLim = 3; //hours
+    this.batteryLim = 6; //hours
     this.data = [];
     this.log = [];
+    this.marginalCashFlow = []
   }
   import(){
+    debugger;
     console.log(process.env.FILEPATH)
     console.log(`${filepath}/${inputFileName}`)
     csv()
@@ -41,45 +71,55 @@ class PriceScheme {
       }
     });
     this.data = simplified.sort((prev, curr) => prev.operatingHour - curr.operatingHour);
-    // Here I'll need to replace the logic wtih 
-    // this.findNextMin(this.data, 0);
-    // console.log('log', this.log);
-    // console.log('AccountBalance', this.calcAccountBalance());
-    // console.log('ending State of Charge', this.calcStateOfCharge());
+
     priceScheme.plan();
     console.log(priceScheme.calcAccountBalance())
     console.log(priceScheme.calcStateOfCharge())
   }
   plan(){ 
     let data = this.data;
-    // Buy first action - spare capacity in battery
-    for (let i = 0; i < this.batteryLim - this.initialBatteryState; i++) {
-      data = this.findNextMin(data, 0);
-    }
     // Sell first action - energy in battery
     for (let i = 0; i < this.initialBatteryState; i++) {
-      data = this.findNextMax(data, 0);
+      const marginalHour = i
+      data = this.findNextMax(data, 0, marginalHour);
+      this.marginalCashFlow.push({
+        value: this.calcAccountBalance(marginalHour),
+        firstMove: 'sell'
+      })
     }
+    // Buy first action - spare capacity in battery
+    for (let i = 0; i < this.batteryLim - this.initialBatteryState; i++) {
+      const marginalHour = i + this.initialBatteryState;
+      data = this.findNextMin(data, 0, marginalHour);
+      this.marginalCashFlow.push({
+        value: this.calcAccountBalance(marginalHour),
+        firstMove: 'buy'
+      })
+    }
+
   }
-  findNextMax(data, i){
+  findNextMax(data, i, marginalHour){
     while (i < data.length && !this.isMax(data, i)) {
       i++
     }
-    data = this.sell(data, i);
+    this.sell(data[i], marginalHour);
+    // remove record from data
+    data.splice(i, 1);
     if (i < data.length) {
-      return this.findNextMin(data, i);
+      return this.findNextMin(data, i, marginalHour);
     } else {
       return data;
     }
   }
-  findNextMin(data, i){
+  findNextMin(data, i, marginalHour){
     while (i < data.length && !this.isMin(data, i)) {
-      this.hold(data, i);
       i++
     }
-    data = this.buy(data, i);
+    this.buy(data[i], marginalHour);
+    // remove record from data
+    data.splice(i, 1);
     if (i < data.length) {
-      return this.findNextMax(data, i);
+      return this.findNextMax(data, i, marginalHour);
     } else {
       return data;
     }
@@ -96,29 +136,32 @@ class PriceScheme {
     const nextPrice = (i + 1 < data.length) ? data[i + 1].price : Infinity;
     return price < prevPrice && price < nextPrice;
   }
-  buy(data, i){
+  buy(hour, marginalHour){
+    if (!hour) {
+      debugger;
+    }
     this.log.push({
       transaction: 'buy',
-      price: data[i].price,
-      operatingHour: data[i].operatingHour
-    })
-    data.splice(i, 1);
-    return data;
+      price: hour.price,
+      operatingHour: hour.operatingHour,
+      marginalHour: marginalHour
+    });
   }
-  sell(data, i){
+  sell(hour, marginalHour){
+    if (!hour) {
+      debugger;
+    }
     this.log.push({
       transaction: 'sell',
-      price: data[i].price * -1,
-      operatingHour: data[i].operatingHour
+      price: hour.price * -1,
+      operatingHour: hour.operatingHour,
+      marginalHour: marginalHour
     });
-    data.splice(i, 1)
-    return data;
   }
-  hold(data, i){
-    return data.splice(i, 0);
-  }
-  calcAccountBalance(){
-    return -1 * this.log.map(hour => hour.price).reduce((accumulator, curr) => {
+  calcAccountBalance(marginalHour){
+    const filtered = this.log.filter( hour => marginalHour === undefined || hour.marginalHour === marginalHour)
+    const prices = filtered.map( hour => hour.price);
+    return -1 * prices.reduce((accumulator, curr) => {
       return accumulator + curr})
   }
   calcStateOfCharge(){
